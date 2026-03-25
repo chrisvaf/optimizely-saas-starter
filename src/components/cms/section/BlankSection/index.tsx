ed257a3ddb6efc02358fbcf9094ca09243a90724
Chris Vafiadis <chrisvaf@gmail.com>
2026-03-25 13:28:52 -0400

    Fix Blank section preview

--- src/components/cms/section/BlankSection/index.tsx
+++ src/components/cms/section/BlankSection/index.tsx
@@ -5,13 +5,9 @@ import { BlankSectionDataFragmentDoc, type BlankSectionDataFragment } from "@/gq
  * Blank Section
  * A section without a predefined layout.
  */
-export const BlankSection_section : CmsComponent<BlankSectionDataFragment> = ({ data, editProps }) => {
-    const componentName = 'Blank Section'
-    const componentInfo = 'A section without a predefined layout.'
-    return <CmsEditable className="w-full border-y border-y-solid border-y-slate-900 py-2 mb-4" {...editProps}>
-        <div className="font-bold italic">{ componentName }</div>
-        <div>{ componentInfo }</div>
-        { Object.getOwnPropertyNames(data).length > 0 && <pre className="w-full overflow-x-hidden font-mono text-sm bg-slate-200 p-2 rounded-sm border border-solid border-slate-900 text-slate-900">{ JSON.stringify(data, undefined, 4) }</pre> }
+export const BlankSection_section : CmsComponent<BlankSectionDataFragment> = ({ editProps, children }) => {
+    return <CmsEditable as="section" className="w-full" {...editProps}>
+        { children }
     </CmsEditable>
 }
 BlankSection_section.displayName = "Blank Section (_section/BlankSection)"
